21facb7c2853987c33548ebada8d8d1a853ad808
Aleksandr <bestprogreraxe@gmail.com>
2022-12-09 02:33:41 +0300

    solved task 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,3 +4,29 @@
 7 -> да
 1 -> нет */
 
+string DayOfTheWeek(int weekDay)
+{
+    if (weekDay == 3)
+    {
+        return "It's not weekend, but it's Wednesday, my dudes.";
+    }    
+    else if (weekDay >= 6)
+    {
+        return "It's weekend. (:";
+    }
+    else 
+    {
+        return "It's not weekend.";
+    }
+}
+
+Console.WriteLine("Enter number of weekday:");
+int weekDay = int.Parse(Console.ReadLine() ?? "");
+if (weekDay > 7 || weekDay < 1)
+{
+    Console.WriteLine($"Error: there is no weekday number {weekDay}.");
+}
+else
+{
+    Console.WriteLine(DayOfTheWeek(weekDay));
+}
