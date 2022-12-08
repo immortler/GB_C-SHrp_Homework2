b2d1842c841945cbc28b1a2de9bc0c04f4ce0273
Aleksandr <bestprogreraxe@gmail.com>
2022-12-09 01:05:37 +0300

    solved task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,3 +4,22 @@
 782 -> 8
 918 -> 1 */
 
+int SecondDigitOfThreeDigNumber(int number)
+{
+    int hundred = number/100;
+    int secondDigit = (number - hundred*100)/10;
+    return secondDigit;    
+}
+
+Console.WriteLine("Enter triple digits number:");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Error: number is not triple digits.");
+}
+else
+{
+    int result = SecondDigitOfThreeDigNumber(number);
+    Console.WriteLine($"Second digit of number {number} is {result}.");
+}
